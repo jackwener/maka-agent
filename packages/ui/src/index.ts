79d2260d41815c76c3bd379de34821553785e088
jackwener <jakevingoo@gmail.com>
2026-05-24 01:27:23 +0800

    Consolidate 2026-05-24 PR patch updates
    Compacted commits:
    - PR-UI-C0 review fixup (@kenji msg 7885a347): renderer-side secondary
    - PR-UI-C0 nit fix (@kenji msg 68ca6bc7): thinking_complete replace

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './components.js';
 export * from './materialize.js';
 export * from './redact.js';
+export * from './thinking-stream.js';
 export * from './toast.js';
 export * from './tool-output-stream.js';
